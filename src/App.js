6b724dafbd7b699d6f46c518041c9d15afeb96cc
Ganesh Singh <krganesh1611@gmail.com>
2022-05-19 11:17:23 +0530

    todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+
+  const [list, updateList] = useState([]);
+  const addNewTask = (task) => {
+    console.log(list.length);
+    const listCopy = [...list]; // copy value by value
+    listCopy.push(task);
+    updateList(listCopy);
+  }
+
+  const handleDelete = (dIdx) => {
+    const filteredContent = list.filter(function (task, idx) {
+      return idx !== dIdx;
+    })
+
+    updateList(filteredContent);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Todo Example</div>
+      <InputTask addNewTask={addNewTask}></InputTask>
+      <TaskList list={list} handleDelete={handleDelete}></TaskList>
+    </>
   );
+}
+
+function InputTask(props) {
+
+  const { addNewTask } = props
+  const [cValue, setValue] = useState("");
+
+  const setCInput = (e) => {
+    console.log(e.target.value);
+    setValue(e.target.value);
+  }
+
+  const setFinalTask = () => {
+    addNewTask(cValue);
+    setValue("");
+  }
+
+  return (
+    <>
+      <div>InputTask</div>
+      <input type="text" value={cValue} onChange={setCInput}></input>
+      <input type="button" value="submit" onClick={setFinalTask}></input>
+    </>
+  )
+}
+
+function TaskList(props) {
+  const { list, handleDelete } = props;
+
+  const deleteTask = (task, idx) => {
+    console.log("Delete called to " + task + " delete this task");
+    handleDelete(idx);
+  }
+  return (
+    <>
+      <div>TaskList</div>
+      <br></br>
+      <h2>==============================================================</h2>
+      <>
+        {
+          list.map(function (task, idx) {
+            return <div key={idx}>
+              <div>{task}</div>
+              <input type="button" value="Delete" onClick={() => {
+                deleteTask(task, idx);
+              }}></input>
+            </div>
+          })
+        }
+      </>
+    </>
+  )
 }
 
 export default App;
