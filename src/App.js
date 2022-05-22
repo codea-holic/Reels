ea243db9cc7f447189610f3b2da943900f28b895
Ganesh Singh <krganesh1611@gmail.com>
2022-05-22 21:57:25 +0530

    Set up of Routing Pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import Feed from './Components/Feed';
+import ForgotPwd from './Components/ForgotPwd';
+import Login from './Components/Login';
+import PageNotFound from './Components/PageNotFound';
+import Profile from './Components/Profile';
+import SignUp from './Components/SignUp';
+import { Routes, Route, Redirect, Navigate } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <div>NavBar</div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/feed" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/forget-pwd" element={<ForgotPwd />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
   )
 }
 
