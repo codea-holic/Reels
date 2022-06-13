bbc7737cdabd07ddcb64aa195caba9fb86eeb421
Ganesh Singh <krganesh1611@gmail.com>
2022-06-13 10:14:32 +0530

    Optimization concept useMemo, useCallBack and React.Memo

--- src/useMemo/Counter.jsx
+++ src/useMemo/Counter.jsx
@@ -1,35 +1,14 @@
 import React from 'react'
-import { useMemo } from 'react';
-function Counter() {
-    const [count, setCount] = React.useState(0);
-    const [count1, setCount1] = React.useState(0);
 
-    const increment = () => (setCount((count) => {
-        return count + 1;
-    }));
-
-    const isEven = useMemo(() => {
-        console.log(count);
-        let i = 0;
-        while (i < 900000000) i++;
-
-        return count % 2 === 0;
-    }, [count]);
-
-    const increment1 = () => (setCount1((count1) => {
-        return count1 + 1;
-    }));
-
-    return (
-        <>
-            <div>Counter</div>
-            <button onClick={increment}>Count</button>
-            <span> {count} {isEven ? "Even" : "Odd"}</span>
-            <br></br>
-            <button onClick={increment1}>Count</button>
-            <span> {count1} </span>
-        </>
-    )
+function Counter(props) {
+    const {count, incrementChild} = props;
+    console.log("Child Rendered");
+  return (
+      <>
+      <div>``````````````````````````````</div>
+      <button onClick={incrementChild}>child Count - {count}</button>
+      </>
+  )
 }
 
-export default Counter;
+export default React.memo(Counter); 
--- src/useMemo/ParentCounter.jsx
+++ src/useMemo/ParentCounter.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
+import React from 'react'
+import { useCallback } from 'react';
+import { useMemo } from 'react';
 import Counter from './Counter';
+
 function ParentCounter() {
+    const [count, setCount] = React.useState(0);
+    const [count1, setCount1] = React.useState(0);
+    console.log("Parent Rendered");
+    const increment = () => (setCount((count) => {
+        return count + 1;
+    }));
+
+    const isEven = useMemo(() => {
+        let i = 0;
+        while (i < 900000000) i++;
+
+        return count % 2 === 0;
+    }, [count]);
+
+    const increment1 = () => (setCount1((count1) => {
+        return count1 + 1;
+    }));
+
+    // I got an warning of exhaustive-deps when we pass parameter
+    // of count in setCount function (means setCount(count));
+
+    const incrementChild = useCallback(() => {
+        setCount(() => {
+            return (count + 1);
+        })
+    }, [count]);
+
+    // useCallBack pure function ko hi yaad kr leta hai -> jab bhi ham function call karte hai
+    // tab wahi function ko excute kr deta hai...
+    /**
+     * useMemo -> jab variable ko memoize krna ho
+     * useCallBack -> jab kisi function ko call krna ho
+     * React.memo -> jab pure component ko use krna ho
+     */
     return (
         <>
-            <div>ParentCounter</div>
-            <Counter></Counter>
+            <button onClick={increment}>Count</button>
+            <span> {count} {isEven ? "Even" : "Odd"}</span>
+            <br></br>
+            <button onClick={increment1}>Count</button>
+            <span> {count1} </span>
+            <Counter count={count} incrementChild={incrementChild}></Counter>
         </>
-
     )
 }
 
