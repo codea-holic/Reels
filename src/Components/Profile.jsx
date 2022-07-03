993f059176865f2e3172a59221ec49821d6e6c88
Ganesh Singh <krganesh1611@gmail.com>
2022-07-03 16:46:51 +0530

    Added profile page html

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import "./profile.css";
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <>
+      <div className="header"></div>
+      <div className="main">
+        {/* <div className="p-img"></div> */}
+        <img src="http://via.placeholder.com/640x360" alt="" className="p-img" />
+        <div className="details">
+          <div className="content name"> Name </div>
+          <div className="content posts"> No. of Posts <span> Posts </span></div>
+          <div className="content mail">Email </div>
+        </div>
+      </div>
+    </>
   )
 }
 
