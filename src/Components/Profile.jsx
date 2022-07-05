71c6bb6939a0ec5a7038ac47332458eb054dd60b
Ganesh Singh <krganesh1611@gmail.com>
2022-07-05 10:27:09 +0530

    Profile styling code

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -7,11 +7,13 @@ function Profile() {
       <div className="header"></div>
       <div className="main">
         {/* <div className="p-img"></div> */}
-        <img src="http://via.placeholder.com/640x360" alt="" className="p-img" />
+        <div className="img-container">
+          <img src="http://via.placeholder.com/640x360" alt="" className="p-img" />
+        </div>
         <div className="details">
-          <div className="content name"> Name </div>
-          <div className="content posts"> No. of Posts <span> Posts </span></div>
-          <div className="content mail">Email </div>
+          <div className="content name"> Name : <span className='bold'> Name </span></div>
+          <div className="content posts"> No. of Posts : <span className='bold'> Posts </span></div>
+          <div className="content mail">Email : <span className='bold'> Email </span> </div>
         </div>
       </div>
     </>
